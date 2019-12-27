1d932d9c906b6358ae36a8644cd085efa78c597a
ZhuMingYang <332595512@qq.com>
2019-12-28 01:03:05 +0800

    模块和组件的异步加载

--- pages/test.jsx
+++ pages/test.jsx
@@ -1,16 +1,21 @@
 import Router, { withRouter } from 'next/router'
 import Link from 'next/link'
+import dynamic from 'next/dynamic'
 import styled from 'styled-components'
+
+// 异步加载组件(渲染到才会加载)
+const Comp = dynamic(import('../components/Comp'))
 
 const Title = styled.h2`
   font-size: 40px;
 `;
 const color = '#0C7D9D'
 
-const Test = ({ router, name }) => {
+const Test = ({ router, name, time }) => {
   return (
     <>
-      <Title>这是标题</Title>
+      <Title>这是标题{ time }</Title>
+      <Comp />
       <Link href="/">
         <h2>{router.query.id} - {name}</h2>
       </Link>
@@ -26,9 +31,11 @@ const Test = ({ router, name }) => {
 
 // page获取数据(刷新此页面会在服务端执行，跳转到此页面会在客户端执行)
 Test.getInitialProps = async () => {
+  const Moment = await import('moment') // 使用webpack懒加载moment
   console.log('-----server render------')
   return {
-    name: 'HAHA'
+    name: 'HAHA',
+    time: Moment.default(Date.now() - 60 * 1000 * 5).fromNow()
   }
 }
 
