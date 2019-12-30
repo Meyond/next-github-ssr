18881e661aca1075ef43772eb0f4d51ee15a62c2
ZhuMingYang <332595512@qq.com>
2019-12-30 11:23:30 +0800

    useState，useEffect， useReducer的使用

--- pages/counter-hooks.jsx
+++ pages/counter-hooks.jsx
@@ -1,35 +1,42 @@
-import { useState, useEffect, useReducer } from 'react'
+import { Button, Input } from "antd";
+import { useState, useEffect, useLayoutEffect, useReducer } from "react";
 
 // 根据action类型对state进行更新
 function countReducer(state, action) {
   switch (action.type) {
-    case 'add':
+    case "add":
       // 如果是对象，需要返使用Object.assign,才能在子组件监听到数据变化
-      return state + 1
-    case 'minus':
-      return state - 1
+      return state + 1;
+    case "minus":
+      return state - 1;
     default:
-      return state
+      return state;
   }
 }
 
 function MyCounter() {
-  const [count, dispatchCount] = useReducer(countReducer, 0)
+  const [count, dispatchCount] = useReducer(countReducer, 0);
+  const [name, setName] = useState("jack");
 
+  // useLayoutEffect会在DOM内容更新之前执行
+  // useEffect会在DOM更新时才会执行
   useEffect(() => {
-    // 初始化、卸载时调用
-    const interval = setInterval(() => {
-      // dispatchCount({ type: 'add' })
-      // or
-      dispatchCount({ type: 'minus' })
-    }, 1000)
+    console.log("effect invoked"); // 组件初始化
+    return () => console.log("effect deteched"); // 组件卸载
+  }, [name]); // 数组内传入的值变化时，会执行useEffect
 
-    return () => clearInterval(interval) // 清除定时器
-  }, [])
-
-  return(
-    <span>计数器：{ count }</span>
-  )
+  return (
+    <div className="hooks">
+      <Input value={name} onChange={e => setName(e.target.value)} type="text" />
+      <Button onClick={() => dispatchCount({ type: "add" })} type="primary">{count}</Button>
+      <style jsx>{`
+        .hooks {
+          width: 300px;
+          padding: 20px;
+        }
+      `}</style>
+    </div>
+  );
 }
 
-export default MyCounter
+export default MyCounter;
