b3b5c22452642d73e9baf70d71e1bbedaf806e44
ZhuMingYang <332595512@qq.com>
2020-01-07 22:16:37 +0800

    使用ReduxThunk支持异步的dispatch ( action )

--- store/store.js
+++ store/store.js
@@ -1,29 +1,54 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk"; // 使用ReduxThunk支持异步的dispatch
 
 const initialState = {
   count: 0
-}
+};
 
-const ADD = 'ADD'
+const ADD = "ADD";
 
 function countReducer(state = initialState, action) {
-  console.log(state, action)
+  console.log(state, action);
   switch (action.type) {
     case ADD:
-      return { count: state.count + 1 }
+      return { count: state.count + (action.num || 1) };
     default:
-      return state
+      return state;
   }
 }
+
 const allReducer = combineReducers({
   count: countReducer
-})
-const store = createStore(allReducer, {
-  count: initialState
-})
+});
+const store = createStore(
+  allReducer,
+  {
+    count: initialState
+  },
+  applyMiddleware(ReduxThunk)
+);
 
 store.subscribe(() => {
-  console.log(store.getState())
-})
-// store.dispatch({ type: ADD })
-export default store
+  console.log(store.getState());
+});
+
+// action creator
+function add(num) {
+  return {
+    type: ADD,
+    num
+  };
+}
+// 异步action
+function addAsync(num) {
+  return (dispatch, getState) => {
+    setTimeout(() => {
+      dispatch(add(num));
+    }, 500);
+  };
+}
+
+store.dispatch(add(3)); // +3
+store.dispatch(addAsync(5)); // +5
+
+export default store;
